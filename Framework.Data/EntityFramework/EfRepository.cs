47ac465277402921300efc37cb56963c6edec397
propellingbits <bullet1128@gmail.com>
2017-04-19 22:39:33 -0700

    Initial Commit

---
+++ Framework.Data/EntityFramework/EfRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Framework.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Framework.Data.EntityFramework
+{
+    public class EfRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+        public EfRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        public DbContext DbContext { get { return _dbContext; } }
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> where = null)
+        {
+            return await _dbSet.CountAsync(where);            
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Framework.Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Framework.Data
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {        
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> where = null);
+        //paging
+        IEnumerable<TEntity> GetPage(params Expression<Func<TEntity, object>>[] includeProperties);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includeProperties);
+        //todo - do we have really have to expose this
+        DbContext DbContext { get; }
+        //todo - provide return type
+        void Validate();
+    }
+}
